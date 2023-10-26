64e503ac9271e0fdeb343c1014a91774acad080b
Iaroslava Finta <iaroslava.sv7@gmail.com>
2023-10-26 14:43:37 -0500

    Updated and working

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
-// const freelancersOption = [
-//     { name: "Dr. Slice", price: 25, occupation: "gardener" },
-//     { name: "Dr. Pressure", price: 51, occupation: "programmer" },
-//     { name: "Prof. Possibility", price: 43, occupation: "teacher" },
-//     { name: "Prof. Prism", price: 81, occupation: "teacher" },
-//     { name: "Dr. Impulse", price: 43, occupation: "teacher" },
-//     { name: "Prof. Spark", price: 76, occupation: "programmer" },
-//     { name: "Dr. Wire", price: 47, occupation: "teacher" },
-//     { name: "Prof. Goose", price: 72, occupation: "driver" },
-// ];
-
-const names = ["Dr. Slice","Dr. Pressure","Prof. Possibility","prof. Prism","Dr. Impulse", "Prof. Spark", "Dr. Wire", "Prof. Goose"];
-const prices = [25, 51, 43, 81, 43, 76, 47, 72];
-const occupations = ["gardener", "programmer", "teacher", "teacher", "teacher", "programmer", "teacher", "driver"];
+const freelancersOption = [
+    { name: "Carol", price: 70, occupation: "programmer" },
+    { name: "Dr. Slice", price: 25, occupation: "gardener" },
+    { name: "Dr. Pressure", price: 51, occupation: "programmer" },
+    { name: "Prof. Possibility", price: 43, occupation: "teacher" },
+    { name: "Prof. Prism", price: 81, occupation: "teacher" },
+    { name: "Dr. Impulse", price: 43, occupation: "teacher" },
+    { name: "Prof. Spark", price: 76, occupation: "programmer" },
+    { name: "Dr. Wire", price: 47, occupation: "teacher" },
+    { name: "Prof. Goose", price: 72, occupation: "driver" },
+];
 
 const freelancers = [
     {
@@ -26,22 +23,22 @@ const freelancers = [
     }
 ];
 
-//`setInterval` will call `addFreelancer` function every 3000 milliseconds (3 second)
+//`setInterval` will call `addFreelancer` function every 5000 milliseconds (5 seconds)
 //setInterval function is created by JS
-const addFreelancerIntervalId = setInterval(addFreelancer,3000);
+const addFreelancerIntervalId = setInterval(addFreelancer,5000);
 
 // We call this function once to render the initial state
 render();
 
 function render() {
-    //const freelancersDemo = document.querySelector("td");
+    const tbody = document.querySelector("tbody");
     const freelancersShow = freelancers.map((freelancer) => {
         const element = document.createElement("tr");
-        element.classList.add("freelancer")
+        //element.classList.add("freelancer");
         element.innerHTML = `
-            <td>Name: ${freelancer.name}</td>
-            <td>Occupation: ${freelancer.occupation}</td>
-            <td>Starting Price: ${freelancer.price}</td>`
+            <td>${freelancer.name}</td>
+            <td>${freelancer.occupation}</td>
+            <td>$${freelancer.price}</td>`
         return element;
     });
     tbody.replaceChildren(...freelancersShow);
@@ -49,20 +46,20 @@ function render() {
 };
 
 function addFreelancer() {
-    const name = names[Math.floor(Math.random() * names.length)];
-    const occupation = occupations[Math.floor(Math.random() * occupations.length)];
-    const price = prices[Math.floor(Math.random() * prices.length)];
-    freelancers.push({ name, occupation, price });
+    //const randomIndex = Math.floor(Math.random() * freelancersOption.length);
+    const randomFreelancer = freelancersOption[0];
+    freelancers.push(randomFreelancer);
+    freelancersOption.shift(randomFreelancer);
     render();
 }
 
 //Create a function for average price
 function averagePrice() {
-    const averagePrice = document.querySelector("h1 span");
-    const calculatePrice = (freelancers.reduce(
-        (accumulator, freelancers) => (accumulator + freelancer.price), 0));
+    const counter = document.querySelector("span");
+    const calculatePrice = freelancers.reduce(
+        (accumulator, currentValue) => (accumulator + currentValue.price), 0);
     const calculatePriceAverage = calculatePrice / freelancers.length;
-    averagePrice.textContent = "$" + calculatePriceAverage;
+    counter.textContent = String(calculatePriceAverage.toFixed(2));
 }
 
 
